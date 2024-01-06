da2ac528206403046c43e4b7c0576520021bca01
Maaz Ibrahim <maazibrahim2004@gmail.com>
2024-01-05 21:07:19 -0500

    Added all task contents
    Added routes for creating, deleting, and updating tasks

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from flask import Flask, render_template, url_for
-# import Flask class from flask package, a class that used to create an instance of a web app
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy # class used to create instance of database
 from datetime import datetime # used to get current date and time
 
@@ -10,18 +9,53 @@ app.app_context().push() # creating an application context, so that we can use t
 
 
 class Todo(db.Model): # class inheriting the base class for all models in SQLAlchemy
-    id = db.Column(db.Integer, primary_key=True) # id column in the database
+    id = db.Column(db.Integer, primary_key=True) # id column in the database. So that each item has a unique id
     content = db.Column(db.String(200), unique = True, nullable = False) # username column
     date_created = db.Column(db.DateTime, default = datetime.utcnow) # date created column
 
     # returns strings whenever we create a new element, by new element we mean a new todo item
     def __repr__(self): # how the object is printed whenever we print it out
-        return '<Task %r>' % self.id
+        return '<Task %r>' % self.id 
     
+# modifying the function below to run on the root path
+@app.route('/', methods=['Post','Get']) # accepts post (send) and get (data) requests methods
+def index(): # request is a parameter that contains all the data that the user sends to the server
+    if request.method == 'POST':
+        task_content = request.form['content'] # if the request method is post, get the content from the form
+        new_task = Todo(content = task_content) # new task object to add to db
+        try:
+            db.session.add(new_task) # add new task to the database
+            db.session.commit() # commit changes to the database
+            return redirect('/') # redirect to the homepage
+        except:
+            return 'There was an issue adding your task'
+    else:
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template('index.html', tasks = tasks) 
 
-@app.route('/')  # modifying the function below to run on the root path
-def index(): 
-    return render_template('index.html') 
+@app.route('/delete/<int:id>') # route to delete a task
+def delete(id):
+    task_to_delete = Todo.query.get_or_404(id) # assign the task with the id passed in the url
+
+    try:
+        db.session.delete(task_to_delete) # delete the task
+        db.session.commit()
+        return redirect('/')
+    except:
+        return 'There was a problem deleting that task'
+    
+@app.route('/update/<int:id>', methods=['GET', 'POST']) # route to update a task
+def update(id):
+    task = Todo.query.get_or_404(id)
+    if request.method == 'POST':
+        task.content = request.form['content']
+        try:
+            db.session.commit()
+            return redirect('/') 
+        except:
+            return 'There was an issue updating your task'
+    else:
+        return render_template('update.html', task = task)
 
 if __name__ == "__main__": # if the script of the app(app.py) is executed directly, run app
     app.run(debug=True, port = 8080)  
